21bd2ee073f585eca538ec6257bf4b28fa1ca1b4
Antiloops <merramacha@gmail.com>
2022-04-21 20:05:03 +0200

    Version 9.1

--- Wild_Fire_Simu/src/Graphics/Grille.java
+++ Wild_Fire_Simu/src/Graphics/Grille.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package graphique;
+package Graphics;
 
 import java.awt.Color;
 import wild_fire_simu.Case;
